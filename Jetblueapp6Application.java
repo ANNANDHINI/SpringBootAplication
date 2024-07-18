206b15e5e0e501956ade551432cee8603d99322f
ANNANDHINI <127080258+ANNANDHINI@users.noreply.github.com>
2024-07-18 14:39:14 +0530

    Add files via upload

---
+++ Jetblueapp6Application.java
@@ -0,0 +1,13 @@
+package com.jetblueapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jetblueapp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jetblueapp6Application.class, args);
+	}
+
+}
