4361bd4f3d45638d00568b80cd01e2712a420223
ANNANDHINI <127080258+ANNANDHINI@users.noreply.github.com>
2024-07-18 14:44:44 +0530

    Add files via upload

---
+++ StudentDto.java
@@ -0,0 +1,41 @@
+package com.jetblueapp6.payload;
+
+public class StudentDto {
+	private long id;
+	private String name;
+	private String branch;
+	private String study;
+	private String marks;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getStudy() {
+		return study;
+	}
+	public void setStudy(String study) {
+		this.study = study;
+	}
+	public String getMarks() {
+		return marks;
+	}
+	public void setMarks(String marks) {
+		this.marks = marks;
+	}
+	
+
+}
